a71f7114f0c6669ab852e9a38966088ffaa6e4b2
青豆 <qingdou@pinduoduo.com>
2020-04-09 21:58:02 +0800

    asset management

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import _ from 'lodash'
+import './style.css'
+import Img from './2.jpg'
 
 function component() {
     const element = document.createElement('div')
     element.innerHTML = _.join(['Hello', 'webpack'], ' ')
+    element.classList.add('hello')
+    const myImg = new Image()
+    myImg.src = Img
+    element.appendChild(myImg)
     return element
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,31 @@
 const path = require('path')
 
-module.export = {
+module.exports = {
     entry: './src/index.js',
     output: {
-        filename: 'main.js',
+        filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|jpg|svg|gif)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            outputPath: 'images'
+                        }
+                    }
+                ]
+            }
+        ]
     }
 }
