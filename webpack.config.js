26d7101d6843e99ffb0e0d383e9e06d025a3d6e8
青豆 <qingdou@pinduoduo.com>
2020-04-13 19:34:14 +0800

    7-AuthoringLibraries-use

--- webpack.config.js
+++ webpack.config.js
@@ -4,16 +4,6 @@ module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'webpack-numbers.js',
-        library: 'webpackNumbers',
-        libraryTarget: 'umd'
-    },
-    externals: {
-        lodash: {
-            commonjs: 'lodash',
-            commonjs2: 'lodash',
-            amd: 'lodash',
-            root: '_'
-        }
+        filename: 'webpack-numbers.js'
     }
 }
