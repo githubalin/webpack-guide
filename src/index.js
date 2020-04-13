dd309319485d0db521a32dd28de949e6a4483f2e
青豆 <qingdou@pinduoduo.com>
2020-04-13 19:13:00 +0800

    7-AuthoringLibraries-use

--- src/index.js
+++ src/index.js
@@ -1,14 +1,3 @@
-import _ from 'lodash';
-import numRef from './ref.json';
+const guide = require('webpack-guide')
 
-export function numToWord(num) {
-    return _.reduce(numRef, (accum, ref) => {
-        return ref.num === num ? ref.word : accum;
-    }, '');
-}
-
-export function wordToNum(word) {
-    return _.reduce(numRef, (accum, ref) => {
-        return ref.word === word && word.toLowerCase() ? ref.num : accum;
-    }, -1);
-}
+console.log(guide.numToWord(1))
