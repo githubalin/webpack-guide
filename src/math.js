ad2120e2da22defc7a9eda63a55e17517d1d7809
青豆 <qingdou@pinduoduo.com>
2020-04-15 16:04:49 +0800

    treeshaking

---
+++ src/math.js
@@ -0,0 +1,13 @@
+export function square(x) {
+    var a = 123123123;
+    var b = 123123123;
+    var c = a + b;
+    return x * x;
+}
+
+export function cube(x) {
+    var a = 123123123;
+    var b = 123123123;
+    var c = a + b;
+    return x * x * x + c;
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
 const path = require('path')
 
-module.export = {
+module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist')
-    }
+    },
+    // mode: 'development',
+    // optimization: {
+    //     usedExports: false,
+    // },
 }
